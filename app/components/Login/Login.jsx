dbe2d44387c80e8a98125074f68231cbebe8f472
Jack Mallahan <jackmallahan@gmail.com>
2017-08-29 16:54:50 -0600

    added api key with git ignore

--- app/components/Login/Login.jsx
+++ app/components/Login/Login.jsx
@@ -1,12 +1,13 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 const Login = () => {
-  return (
-    <div className="login-div">
-      <input placeholder="Username" className="username-input" />
-      <input placeholder="Password" className="password-input" />
-    </div>
-  );
-};
+	return (
+		<div className="login-div">
+			<input placeholder="Username" className="username-input" />
+			<input placeholder="Password" className="password-input" />
+			<input type="submit" onClick={() => alert('you pushed the button')} />
+		</div>
+	)
+}
 
-export default Login;
+export default Login
---
+++ app/utils/APIkey.js
@@ -0,0 +1,1 @@
+export default const key = '8b8eafe65704813392f88b71fcdd215c'
