1ce5a5bcb05151c13e7af6a9810feafab49ad938
Jonathan Beckman <jbecks.media@gmail.com>
2017-08-30 10:25:10 -0600

    start building out thunk logic

--- app/index.js
+++ app/index.js
@@ -1,21 +1,40 @@
-import { createStore, applyMiddleware } from 'redux'
-import App from './components/App/App'
-// import './main.css'
-import thunk from 'redux-thunk'
-import rootReducer from './reducers'
-import key from './utils/APIkey'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Provider from 'react-redux'
+import { createStore } from 'redux'
+import { createThunkMiddleware, applyMiddlewater } from './redux-thunk-local'
+import { Provider } from 'react-redux'
+// import './main.css'
+// import thunk from 'redux-thunk'
+import key from './utils/APIkey'
+
+import rootReducer from './reducers'
+import App from './components/App/App'
+
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const thunk = createThunkMiddleware();
 
 const store = createStore(rootReducer, applyMiddleware(thunk), devTools)
 
-function fetchNowPlaying() {
-	fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=$%7Bpage%7D`)
-		.then(data => data.json())
-		.then(data => console.log(data))
+//==========================
+
+function createThunkMiddleware(extraArgument) {
+  return ({ dispatch, getState }) => next => action => {
+    if (typeof action === 'function') {
+      return action(dispatch, getState, extraArgument);
+    }
+return next(action);
+  };
 }
+thunk.withExtraArgument = createThunkMiddleware;
+export default thunk;
+//==========================
+
+// function fetchNowPlaying() {
+// 	fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=$%7Bpage%7D`)
+// 		.then(data => data.json())
+// 		.then(data => console.log(data))
+// }
 
 ReactDOM.render(
 	<Provider store={store}>
