f66f03a81310da48b047055613482a10fa861476
Jack Mallahan <jackmallahan@gmail.com>
2017-08-31 12:41:27 -0600

    register sends data to backend

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -1,50 +1,50 @@
 export const getUser = newUser => {
-  return {
-    type: "ADD_USER",
-    newUser
-  };
-};
+	return {
+		type: 'ADD_USER',
+		newUser
+	}
+}
 
 export const saveData = data => {
-  return {
-    type: "SAVE_DATA",
-    data: data
-  };
-};
+	return {
+		type: 'SAVE_DATA',
+		data: data
+	}
+}
 
 export const login = userInfo => {
-  return {
-    type: "LOGIN",
-    userInfo
-  };
-};
+	return {
+		type: 'LOGIN',
+		userInfo
+	}
+}
 
 export const register = userInfo => {
 	return {
-		type: "REGISTER",
+		type: 'REGISTER',
 		userInfo
-	};
-};
+	}
+}
 
 export const getNowPlaying = url => {
-  return dispatch => {
-    fetch(url)
-      .then(data => data.json())
-      .then(data => dispatch(saveData(data.results)));
-  };
-};
+	return dispatch => {
+		fetch(url)
+			.then(data => data.json())
+			.then(data => dispatch(saveData(data.results)))
+	}
+}
 
 export const postNewUser = user => {
-  return dispatch => {
-    fetch("/api/users/new", {
-      method: "POST",
-      body: JSON.stringify(user),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-      .then(data => data.json())
-      .then(parsedData => dispatch(register(parsedData)))
-      .catch(e => console.log(e));
-  };
-};
+	return dispatch => {
+		fetch('/api/users/new', {
+			method: 'POST',
+			body: JSON.stringify(user),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+			.then(data => data.json())
+			.then(parsedData => dispatch(addUser(parsedData)))
+			.catch(e => console.log(e))
+	}
+}
