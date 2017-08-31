791cdaca583a8bba3d3d7b1d3ecd5b0d600b4244
Tyler Hevia <tylerhevia@gmail.com>
2017-08-31 16:17:57 -0600

    added favorites route, hooked up login action

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -1,70 +1,80 @@
 export const getUsers = users => {
-	return {
-		type: 'GET_USERS',
-		users
-	}
-}
+  return {
+    type: "GET_USERS",
+    users
+  };
+};
 
 export const saveData = data => {
-	return {
-		type: 'SAVE_DATA',
-		data: data
-	}
-}
+  return {
+    type: "SAVE_DATA",
+    data: data
+  };
+};
 
 export const login = userInfo => {
-	return {
-		type: 'LOGIN',
-		userInfo
-	}
-}
+  return {
+    type: "LOGIN",
+    userInfo
+  };
+};
 
 export const register = userInfo => {
-	return {
-		type: 'REGISTER',
-		userInfo
-	}
-}
+  return {
+    type: "REGISTER",
+    userInfo
+  };
+};
 
 export const getNowPlaying = url => {
-	return dispatch => {
-		fetch(url)
-			.then(data => data.json())
-			.then(data => dispatch(saveData(data.results)))
-	}
-}
+  return dispatch => {
+    fetch(url)
+      .then(data => data.json())
+      .then(data => dispatch(saveData(data.results)));
+  };
+};
 
-export const checkUser = user => {
-	// if (check store against user email and password) {
-	// 	//if true then allow login
-	// } else {
-	// 	//dont allow login
-	// }
-}
+export const signIn = user => {
+  console.log("user", user);
+  return dispatch => {
+    fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(data => data.json())
+      .then(data => {
+        console.log(data);
+        return dispatch(login(data));
+      });
+  };
+};
 
 export const postNewUser = user => {
-	return dispatch => {
-		fetch('/api/users/new', {
-			method: 'POST',
-			body: JSON.stringify(user),
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		})
-			.then(data => {
-				data.ok ? data.json() : alert('User Already Exists')
-			})
-			.then(parsedData => {
-				dispatch(register(parsedData))
-			})
-			.catch(err => console.log('in catch ', err))
-	}
-}
+  return dispatch => {
+    fetch("/api/users/new", {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(data => {
+        data.ok ? data.json() : alert("User Already Exists");
+      })
+      .then(parsedData => {
+        dispatch(register(parsedData));
+      })
+      .catch(err => console.log("in catch ", err));
+  };
+};
 
 export const retrieveUsers = users => {
-	return dispatch => {
-		fetch('/api/users')
-			.then(data => data.json())
-			.then(data => dispatch(getUsers(data.data)))
-	}
-}
+  return dispatch => {
+    fetch("/api/users")
+      .then(data => data.json())
+      .then(data => dispatch(getUsers(data.data)));
+  };
+};
