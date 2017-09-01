f7b52085ea417fb09f4c14ce4fcae9fcf4d62b62
Tyler Hevia <tylerhevia@gmail.com>
2017-09-01 17:02:07 -0600

    add to favorites on click

--- app/components/MovieCard/MovieCard.jsx
+++ app/components/MovieCard/MovieCard.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
+import React from "react";
 
-const MovieCard = ({ movie }) => {
-	const { title, overview, backdrop_path, poster_path } = movie
-	const photo = backdrop_path ? backdrop_path : poster_path
-	return (
-		<div
-			className="movie-card"
-			style={{
-				backgroundImage: `url(https://image.tmdb.org/t/p/w1280/${photo})`
-			}}
-		>
-			<h3>
-				{title}
-				<button>Favorite</button>
-			</h3>
-			<p>{overview}</p>
-		</div>
-	)
-}
+const MovieCard = ({ movie, addFavorite, user }) => {
+  const { title, overview, backdrop_path, poster_path } = movie;
+  const photo = backdrop_path ? backdrop_path : poster_path;
+  return (
+    <div
+      className="movie-card"
+      style={{
+        backgroundImage: `url(https://image.tmdb.org/t/p/w1280/${photo})`
+      }}
+      onClick={() =>
+        addFavorite({
+          movie_id: movie.id,
+          title: movie.title,
+          user_id: user.id,
+          poster_path: movie.poster_path,
+          release_date: movie.release_date,
+          vote_average: movie.vote_average,
+          overview: movie.overview
+        })}
+    >
+      <h3>
+        {title}
+        <button>Favorite</button>
+      </h3>
+      <p>
+        {overview}
+      </p>
+    </div>
+  );
+};
 
-export default MovieCard
+export default MovieCard;
