01bd7b55ba4645dd70841b126283df14f0dd9c3b
Jonathan Beckman <jbecks.media@gmail.com>
2017-09-02 12:39:17 -0600

    add redirect functionality at login page

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux'
-import users from './userReducer'
 import loggedIn from './loginReducer'
 import favorites from '../components/Favorites/favoritesReducer'
 import filmsNowPlaying from './filmsNowPlaying'
 
 const rootReducer = combineReducers({
-	users,
 	filmsNowPlaying,
 	loggedIn,
 	favorites
