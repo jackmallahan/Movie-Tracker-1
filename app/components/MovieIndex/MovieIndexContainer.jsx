5f7f5b3c9ce881b6b009a940bf286331e7f7ee57
Tyler Hevia <tylerhevia@gmail.com>
2017-09-02 13:28:00 -0600

    logout, store current user to local storage, not retrieving local user yet

--- app/components/MovieIndex/MovieIndexContainer.jsx
+++ app/components/MovieIndex/MovieIndexContainer.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { getNowPlaying, addFavorite } from '../../actions/actions.jsx'
-import MovieIndex from './MovieIndex.jsx'
-import { bindActionCreators } from 'redux'
+import React from "react";
+import { connect } from "react-redux";
+import { getNowPlaying, addFavorite } from "../../actions/actions.jsx";
+import MovieIndex from "./MovieIndex.jsx";
+import { bindActionCreators } from "redux";
 
 const mapStateToProps = state => {
-	return { filmsNowPlaying: state.filmsNowPlaying }
-}
+  return {
+    filmsNowPlaying: state.filmsNowPlaying,
+    currentUser: state.loggedIn
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-	return bindActionCreators({ loadNowPlaying: getNowPlaying }, dispatch)
-}
-export default connect(mapStateToProps, mapDispatchToProps)(MovieIndex)
+  return bindActionCreators({ loadNowPlaying: getNowPlaying }, dispatch);
+};
+export default connect(mapStateToProps, mapDispatchToProps)(MovieIndex);
