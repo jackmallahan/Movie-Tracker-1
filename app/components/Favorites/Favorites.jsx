2f643352d3ca7396aab8f5d7c8e58738dfc4c185
Jack Mallahan <jackmallahan@gmail.com>
2017-09-03 20:04:08 -0600

    removes favorite if favorite movie card is clicked

--- app/components/Favorites/Favorites.jsx
+++ app/components/Favorites/Favorites.jsx
@@ -1,41 +1,35 @@
-import React, { Component } from "react";
-import MovieCardContainer from "../MovieCard/MovieCardContainer.js";
+import React, { Component } from 'react'
+import MovieCardContainer from '../MovieCard/MovieCardContainer.js'
 
 class Favorites extends Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props)
+	}
 
-  componentDidMount() {
-    let loadedUser;
+	componentDidMount() {
+		let loadedUser
 
-    localStorage.getItem("currentUser") !== "undefined" || null
-      ? (loadedUser = JSON.parse(localStorage.getItem("currentUser")))
-      : console.log("no user stored");
+		localStorage.getItem('currentUser') !== 'undefined' || null
+			? (loadedUser = JSON.parse(localStorage.getItem('currentUser')))
+			: console.log('no user stored')
 
-    if (loadedUser) {
-      this.props.login(loadedUser);
-      return this.props.getAllFavorites(loadedUser.id);
-    }
-  }
+		if (loadedUser) {
+			this.props.login(loadedUser)
+			return this.props.getAllFavorites(loadedUser.id)
+		}
+	}
 
-  render() {
-    let mappedFaves;
+	render() {
+		let mappedFaves
 
-    if (this.props.faves[0]) {
-      mappedFaves = this.props.faves.map(movie =>
-        <MovieCardContainer key={movie.id} movie={movie} />
-      );
-    } else {
-      mappedFaves = <div className="sweet-action">Sweet Action</div>;
-    }
+		if (this.props.faves[0]) {
+			mappedFaves = this.props.faves.map(movie => <MovieCardContainer key={movie.id} movie={movie} />)
+		} else {
+			mappedFaves = <div className="sweet-action">Sweet Action</div>
+		}
 
-    return (
-      <div className="faves-list">
-        {mappedFaves}
-      </div>
-    );
-  }
+		return <div className="faves-list">{mappedFaves}</div>
+	}
 }
 
-export default Favorites;
+export default Favorites
