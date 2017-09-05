756dad302514fb271cff5f19662752218b10cdee
Jack Mallahan <jackmallahan@gmail.com>
2017-09-05 00:05:05 -0600

    got wrapper to work

--- app/components/App/App.test.js
+++ app/components/App/App.test.js
@@ -1,17 +1,17 @@
-import React from "react";
-import App from "./App";
-import { shallow, mount, render } from "enzyme";
-import fetchMock from "fetch-mock";
+import React from 'react'
+import App from './App'
+import { shallow, mount, render } from 'enzyme'
+import fetchMock from 'fetch-mock'
 
-describe("App", () => {
-  let wrapper;
-  it.only("should render", () => {
-    expect(true);
-  });
+describe('App', () => {
+	let wrapper
+	it('true should equal true', () => {
+		expect(true)
+	})
 
-  it("should render a div", () => {
-    wrapper = shallow(<App />);
+	it('should render a div', () => {
+		wrapper = shallow(<appContainer />)
 
-    console.log(wrapper);
-  });
-});
+		console.log(wrapper)
+	})
+})
