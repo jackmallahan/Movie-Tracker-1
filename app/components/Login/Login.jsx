2c3a4baae1fcde94bd0b30adc3959f41c457c427
Jack Mallahan <jackmallahan@gmail.com>
2017-09-19 06:34:09 -0600

    added ability to remove gear

--- app/components/Login/Login.jsx
+++ app/components/Login/Login.jsx
@@ -1,67 +1,67 @@
-import React, { Component } from "react";
-import { Route } from "react-router-dom";
-import { Redirect } from "react-router";
-import MovieIndexContainer from "../MovieIndex/MovieIndexContainer.jsx";
-import RegisterContainer from "../Register/RegisterContainer";
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import { Redirect } from 'react-router'
+import MovieIndexContainer from '../MovieIndex/MovieIndexContainer.jsx'
+import RegisterContainer from '../Register/RegisterContainer'
 
 class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: ""
-    };
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			email: '',
+			password: ''
+		}
+	}
 
-  handleInput(type, input) {
-    this.setState({
-      [type]: input
-    });
-  }
+	handleInput(type, input) {
+		this.setState({
+			[type]: input
+		})
+	}
 
-  render() {
-    if (this.props.currentUser.id) {
-      return <Redirect to="/" />;
-    }
+	render() {
+		if (this.props.currentUser.id) {
+			return <Redirect to="/" />
+		}
 
-    return (
-      <div className="login-div">
-        <h2 className="login-header">Log in to an existing account</h2>
-        <form
-          onSubmit={e => {
-            e.preventDefault();
-            this.props.handleSubmit({
-              email: this.state.email.toLowerCase(),
-              password: this.state.password
-            });
-            this.setState({
-              email: "",
-              password: ""
-            });
-          }}
-        >
-          <input
-            placeholder="Email"
-            className="email-input"
-            onChange={e => this.handleInput("email", e.target.value)}
-          />
+		return (
+			<div className="login-div">
+				<h2 className="login-header">Log in to an existing account</h2>
+				<form
+					onSubmit={e => {
+						e.preventDefault()
+						this.props.handleSubmit({
+							email: this.state.email.toLowerCase(),
+							password: this.state.password
+						})
+						this.setState({
+							email: '',
+							password: ''
+						})
+					}}
+				>
+					<input
+						placeholder="Email"
+						className="email-input"
+						onChange={e => this.handleInput('email', e.target.value)}
+					/>
 
-          <input
-            placeholder="Password"
-            type="password"
-            className="password-input"
-            onChange={e => this.handleInput("password", e.target.value)}
-          />
+					<input
+						placeholder="Password"
+						type="password"
+						className="password-input"
+						onChange={e => this.handleInput('password', e.target.value)}
+					/>
 
-          <input
-            className="login-button"
-            type="submit"
-            disabled={!this.state.email || !this.state.password ? true : false}
-          />
-        </form>
-      </div>
-    );
-  }
+					<input
+						className="login-button"
+						type="submit"
+						disabled={!this.state.email || !this.state.password ? true : false}
+					/>
+				</form>
+			</div>
+		)
+	}
 }
 
-export default Login;
+export default Login
--- app/components/MovieIndex/MovieIndexContainer.jsx
+++ app/components/MovieIndex/MovieIndexContainer.jsx
@@ -1,30 +1,25 @@
-import React from "react";
-import { connect } from "react-redux";
-import {
-  getNowPlaying,
-  addFavorite,
-  getAllFavorites,
-  login
-} from "../../actions/actions.jsx";
-import MovieIndex from "./MovieIndex.jsx";
-import { bindActionCreators } from "redux";
+import React from 'react'
+import { connect } from 'react-redux'
+import { getNowPlaying, addFavorite, getAllFavorites, login } from '../../actions/actions.jsx'
+import MovieIndex from './MovieIndex.jsx'
+import { bindActionCreators } from 'redux'
 
 const mapStateToProps = state => {
-  return {
-    filmsNowPlaying: state.filmsNowPlaying,
-    currentUser: state.loggedIn,
-    favorites: state.favorites
-  };
-};
+	return {
+		filmsNowPlaying: state.filmsNowPlaying,
+		currentUser: state.loggedIn,
+		favorites: state.favorites
+	}
+}
 
 const mapDispatchToProps = dispatch => {
-  return bindActionCreators(
-    {
-      loadNowPlaying: getNowPlaying,
-      getAllFavorites: getAllFavorites,
-      login: login
-    },
-    dispatch
-  );
-};
-export default connect(mapStateToProps, mapDispatchToProps)(MovieIndex);
+	return bindActionCreators(
+		{
+			loadNowPlaying: getNowPlaying,
+			getAllFavorites: getAllFavorites,
+			login: login
+		},
+		dispatch
+	)
+}
+export default connect(mapStateToProps, mapDispatchToProps)(MovieIndex)
